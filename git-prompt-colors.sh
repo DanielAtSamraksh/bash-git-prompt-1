df9a247113131941d75a59b23468b5e867e3c856
Dan <dan@dan-VirtualBox.(none)>
2014-05-18 22:44:10 -0400

    Get rid of symbols. They overlap the text that follows.

--- git-prompt-colors.sh
+++ git-prompt-colors.sh
@@ -8,13 +8,13 @@ GIT_PROMPT_SEPARATOR="|"              # separates each item
 
 # GIT_PROMPT_BRANCH="${Magenta}"        # the git branch that is active in the current directory
 GIT_PROMPT_BRANCH=""        # the git branch that is active in the current directory
-GIT_PROMPT_STAGED="●"           # the number of staged files/directories
-GIT_PROMPT_CONFLICTS="✖"        # the number of files in conflict
-GIT_PROMPT_CHANGED="✚"         # the number of changed files
+GIT_PROMPT_STAGED="S"           # the number of staged files/directories
+GIT_PROMPT_CONFLICTS="x"        # the number of files in conflict
+GIT_PROMPT_CHANGED="+"         # the number of changed files
 
 GIT_PROMPT_REMOTE=" "                 # the remote branch name (if any) and the symbols for ahead and behind
 GIT_PROMPT_UNTRACKED="…"       # the number of untracked files/dirs
-GIT_PROMPT_STASHED="⚑"     # the number of stashed files/dir
+GIT_PROMPT_STASHED="H"     # the number of stashed files/dir
 GIT_PROMPT_CLEAN="✔"      # a colored flag indicating a "clean" repo
 
 GIT_PROMPT_START_USER="${PathShort}"
@@ -23,6 +23,6 @@ GIT_PROMPT_END_USER=" $ "
 GIT_PROMPT_END_ROOT=" # "
 
 # Please do not add colors to these symbols
-GIT_PROMPT_SYMBOLS_AHEAD="↑·"         # The symbol for "n versions ahead of origin"
-GIT_PROMPT_SYMBOLS_BEHIND="↓·"        # The symbol for "n versions behind of origin"
+GIT_PROMPT_SYMBOLS_AHEAD="+"         # The symbol for "n versions ahead of origin"
+GIT_PROMPT_SYMBOLS_BEHIND="-"        # The symbol for "n versions behind of origin"
 GIT_PROMPT_SYMBOLS_PREHASH=":"        # Written before hash of commit, if no name could be found
